7065231bbf482a30a6d591ef78358495395e1241
Andrew Scribner <ca.scribner@gmail.com>
2019-06-11 12:06:25 -0400

    Update handling of logger for lrl package

--- lrl/__init__.py
+++ lrl/__init__.py
@@ -3,15 +3,11 @@ from .data_stores import *
 from .utils import *
 from .solvers import *
 
-# FUTURE: Am I handling logger correctly?  See refs:
-#   https://stackoverflow.com/questions/27016870/how-should-logging-be-used-in-a-python-package
-#   https://docs.python.org/3.7/howto/logging.html#configuring-logging-for-a-library
-#   https://github.com/kennethreitz/requests/blob/master/requests/__init__.py
-
 # Taken from Requests:
 # Set default logging handler to avoid "No handler found" warnings.
 # Recommendation from python docs and using Requests package as a template
 import logging
 from logging import NullHandler
 
-logging.getLogger(__name__).addHandler(NullHandler())
+logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
